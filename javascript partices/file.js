08d6248decb772ca18e33392aa0ba507adac68c2
Kalyani wasane <144616357+hgeydkalyani@users.noreply.github.com>
2026-01-07 13:29:59 +0530

    Add files via upload

---
+++ javascript partices/file.js
@@ -0,0 +1,12 @@
+console.log("Hello, World!");
+let fullname="John Doe";
+console.log(fullname);
+const product={
+     productName:String("Laptop"),
+    productPrice:Number(1500),
+    productAvailable:Boolean(true),
+    productRsting:Number(4.5)
+    
+
+}
+console.log(product);
